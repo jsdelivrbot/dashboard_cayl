a2d34e5c1a7225e11c0e95b9d0c03f6c4340190b
waganse <yosuke.sato@datorama.com>
2018-05-28 10:35:21 +0900

    mod

--- js/lib/header/lib.js
+++ js/lib/header/lib.js
@@ -1,7 +1,7 @@
 (function() {
 
     var tpl = _.template('<header class="header"><svg class="brand-logo"><use xlink:href="#svg-logo1" /></svg><div class="sprite sprite-logo"><img src="https://cdn.rawgit.com/waganse/src_bank/77d00d14/img/t.png" alt=""></div></header>'),
-        $parentNode = $('#pageData', parent.document),
+        $pageNode = $('#pageData', parent.document),
         $customNode = $pageNode.find('#custom-dom-header');
 
     // Render header
