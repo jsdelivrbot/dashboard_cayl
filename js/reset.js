f0c01fd25b607d178ced1c716b861ae7c87cf504
waganse <yosuke.sato@datorama.com>
2018-05-28 15:05:23 +0900

    mod

--- js/reset.js
+++ js/reset.js
@@ -11,7 +11,7 @@
       }),
       $style01 = $('<link>', {
         rel: 'stylesheet',
-        href: 'https://cdn.rawgit.com/waganse/dashboard_cayl/02db4dc9/css/reset.css'
+        href: 'https://cdn.rawgit.com/waganse/dashboard_cayl/8f17dda2/css/reset.css'
       }),
       $style02 = $('<link>', {
         rel: 'stylesheet',
