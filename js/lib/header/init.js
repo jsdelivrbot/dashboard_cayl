ca1394276685ac681e8df23b4d84851253be4305
waganse <yosuke.sato@datorama.com>
2018-05-28 10:27:16 +0900

    mod

--- js/lib/header/init.js
+++ js/lib/header/init.js
@@ -6,13 +6,49 @@
     #page #custom-dom-header {
       position: fixed;
       top: 50px;
-      right: 0;
       width: 100%;
-      height: 80px;
+      height: 60px;
+      line-height: 60px;
       background: url(https://cdn.rawgit.com/waganse/src_bank/50b2df10/img/bg_white_transparent70.png) repeat;
-      box-shadow: 2px 2px 2px #aaa;
+      -webkit-box-shadow: 0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12);
+          box-shadow: 0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12);
       z-index: 9999;
     }
+    #page #custom-dom-header .header .brand-logo {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      -webkit-transform: scale(0.3) translate(-50%, -50%);
+          -ms-transform: scale(0.3) translate(-50%, -50%);
+              transform: scale(0.3) translate(-50%, -50%);
+      -webkit-transform-origin: left top;
+          -ms-transform-origin: left top;
+              transform-origin: left top;
+    }
+    #page #custom-dom-header .header .sprite-logo {
+      position: absolute;
+      top: 50%;
+      right: 20px;
+      -webkit-transform: scale(0.4) translateY(-50%);
+          -ms-transform: scale(0.4) translateY(-50%);
+              transform: scale(0.4) translateY(-50%);
+      -webkit-transform-origin: right top;
+          -ms-transform-origin: right top;
+              transform-origin: right top;
+    }
+    #page #custom-dom-header .sprite-logo {
+      background-image: url("/img/sprite.png");
+      background-position: 0px 0px;
+      width: 197px;
+      height: 73px;
+    }
+    #page #custom-dom-header .sprite-icon_top {
+      background-image: url("/img/sprite.png");
+      background-position: 0px -73px;
+      width: 83px;
+      height: 83px;
+    }
+
   `,
   customCssText = window.css || '',
   $parentNode = $('#pageData', parent.document),
--- js/lib/header/lib.js
+++ js/lib/header/lib.js
@@ -1,6 +1,6 @@
 (function() {
 
-    var tpl = _.template('<div><svg class="sprite-svg"><use xlink:href="#svg-logo1"></div><div class="custom-menu js-menu-container"><a class="custom-menu__trigger js-dropdown-trigger" href="#" data-target="pagemenu-list"><span class="glyphicon glyphicon-menu-hamburger"></span></a><div class="custom-menu__overlay js-overlay"></div><ul id="pagemenu-list" class="custom-menu__list js-dropdown-target"></ul></div>'),
+    var tpl = _.template('<header class="header"><svg class="brand-logo"><use xlink:href="#svg-logo1" /></svg><div class="sprite sprite-logo"><img src="https://cdn.rawgit.com/waganse/src_bank/77d00d14/img/t.png" alt=""></div></header>'),
         $parentNode = $('#pageData', parent.document);
 
     // Render header
