b7f86b5f93da5a70d0e65fdf48d1183130965e55
waganse <yosuke.sato@datorama.com>
2018-05-28 14:51:26 +0900

    mod

--- js/lib/header/lib.js
+++ js/lib/header/lib.js
@@ -1,6 +1,6 @@
 (function() {
 
-    var tpl = _.template('<header class="header"><svg class="brand-logo"><use xlink:href="#svg-logo2" /></svg><div class="sprite sprite-logo"><img src="https://cdn.rawgit.com/waganse/src_bank/77d00d14/img/t.png" alt=""></div></header>'),
+    var tpl = _.template('<header class="header"><svg class="brand-logo"><use xlink:href="#svg-logo1" /></svg><div class="sprite sprite-logo"><img src="https://cdn.rawgit.com/waganse/src_bank/77d00d14/img/t.png" alt=""></div></header>'),
         $pageNode = $('#pageData', parent.document),
         $customNode = $pageNode.find('#custom-dom-header');
 
