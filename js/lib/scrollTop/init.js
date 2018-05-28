07a622ced504d986346da961f3a5aa18ce09e3dc
waganse <yosuke.sato@datorama.com>
2018-05-28 10:56:53 +0900

    mod

--- js/lib/scrollTop/init.js
+++ js/lib/scrollTop/init.js
@@ -3,11 +3,14 @@
   var cssText = `
     #pageData #custom-dom-scrolltop {
       display: block;
+      width: 60px;
+      height: 60px;
       position: fixed;
       bottom: 30px;
       right: 30px;
-      background: url(https://cdn.rawgit.com/waganse/dashboard_cayl/81109573/img/icon_top.png);
+      background: url(https://cdn.rawgit.com/waganse/dashboard_cayl/81109573/img/icon_top.png) no-repeat;
       z-index: 9999;
+      background-size: 60px;
       transform: translateY(150%);
       transition: all .5s;
       cursor: pointer;
