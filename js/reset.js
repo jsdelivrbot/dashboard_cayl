89d4a933b85a02789aa1bccda7574c8482892360
waganse <yosuke.sato@datorama.com>
2018-05-31 12:53:55 +0900

    mod on pageData margin-top

--- js/reset.js
+++ js/reset.js
@@ -11,7 +11,7 @@
       }),
       $style01 = $('<link>', {
         rel: 'stylesheet',
-        href: 'https://cdn.rawgit.com/waganse/dashboard_cayl/d043923/css/reset.css'
+        href: 'https://cdn.rawgit.com/waganse/dashboard_cayl/2b9ac17/css/reset.css'
       });
       // $style02 = $('<link>', {
       //   rel: 'stylesheet',
