c56fc223fe470da61b23afea210b01082c087ea2
waganse <yosuke.sato@datorama.com>
2018-06-08 17:38:19 +0900

    mod on scroll

--- js/lib/scrollTop/init.js
+++ js/lib/scrollTop/init.js
@@ -21,6 +21,27 @@
     #pageData #custom-dom-scrolltop.active {
       transform: translateY(0);
     }
+
+    .grid-tab #custom-dom-scrolltop {
+      display: block;
+      width: 60px;
+      height: 60px;
+      position: fixed;
+      bottom: 30px;
+      right: 30px;
+      background: url(https://cdn.rawgit.com/waganse/dashboard_cayl/81109573/img/icon_top.png) no-repeat;
+      z-index: 9999;
+      background-size: 60px;
+      transform: translateY(150%);
+      transition: all .5s;
+      cursor: pointer;
+    }
+    .grid-tab #custom-dom-scrolltop:hover {
+      opacity: .6;
+    }
+    .grid-tab #custom-dom-scrolltop.active {
+      transform: translateY(0);
+    }
   `,
   newUIFlag = ($('#pageData', parent.document).length)? false : true,
   $parentNode = (newUIFlag)? $('.grid-tab', parent.document) : $('#pageData', parent.document),
