e80a082f8a2583a1e93da053884b8a3eb1512bd0
waganse <yosuke.sato@datorama.com>
2018-06-08 17:01:58 +0900

    remove config

--- js/reset.js
+++ js/reset.js
@@ -3,51 +3,24 @@
   var cssText = `
   `;
   ///////////////////////////////////////////////////////////
-  var svgPath = 'https://cdn.rawgit.com/waganse/src_bank/50b2df10/svg/cayl/svg-symbols.svg',
-      $parentBody = $('body', parent.document),
+  var $parentBody = $('body', parent.document),
       $parentNode = $('#pageData', parent.document),
       $customNode = $('<div>', {
         id: 'custom-dom'
       }),
-      $style01 = $('<link>', {
+      $style = $('<link>', {
         rel: 'stylesheet',
         href: 'https://cdn.rawgit.com/waganse/dashboard_cayl/dc70a9f/css/reset.css'
       });
-      // $style02 = $('<link>', {
-      //   rel: 'stylesheet',
-      //   href: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'
-      // });
 
   // Init
   $parentBody.find('#custom-dom').remove();
 
-  // SVG
-  $.get(svgPath, function(data) {
-    $parentBody.prepend(new XMLSerializer().serializeToString(data.documentElement));
-  });
-
   // Custom DOM
   $parentNode.append($customNode);
 
-  // Overlay
-  $customNode.append('<div class="custom__overlay js-overlay"></div>');
-
-  // Config switcher
-  $customNode.append('<div class="custom__config js-config"><i class="dicon-wrench"></i></div>');
-  $customNode.find('.js-config').on('click', function() {
-    $parentNode.toggleClass('config');
-
-    return false;
-  });
-
-
   // CSS insert
   $style01.html(cssText);
-  $parentNode.append($style01);
-  // $parentNode.append($style02);
-
-  $customNode.find('.js-overlay').on('click', function() {
-    $parentNode.removeClass('config');
-  });
+  $parentNode.append($style);
 
 })();
